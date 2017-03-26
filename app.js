86f6836b7841013db367276647bc83160ac8c867
LokeshS571 <lokeshv542@gmail.com>
2017-03-26 14:27:15 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+var a=25,b=16,c=7,big;
+if(a>b)
+{
+	if(a>c)
+	big=a;
+else
+	big=c;
+}
+else
+{
+	if(b>c)
+		big=b;
+	else
+		big=c;
+}
+
+
+// to display the area in the console
+console.log("The greatest of the three numbers ",a,b,c,"is ",big);
+ 
+
